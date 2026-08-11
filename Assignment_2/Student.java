1341f1f3fa18ffda4ef11824a13f062a110342ca
AaryanHarshalPawar <89315104+AaryanHarshalPawar@users.noreply.github.com>
2026-08-11 12:21:30 +0530

    Implement Student class with constructors and display method

--- Assignment_2/Student.java
+++ Assignment_2/Student.java
@@ -1,1 +1,30 @@
+package Assignment_2;
 
+public class Student{
+    String studentName;
+    int rollNumber;
+
+    Student(){
+        studentName = "Not Assigned";
+        rollNumber = 0;
+    }
+
+    Student(String studentName, int rollNumber){
+        this.studentName = studentName;
+        this.rollNumber = rollNumber;
+    }
+    void display(){
+        System.out.println("Student name: "+studentName + " Rollno: "+rollNumber);
+    }
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student("Aaryan", 519);
+
+        System.out.println("Student initialized using default constructor");
+        s1.display();
+        
+        System.out.println("Student initialized using parameterized constructor");
+        s2.display();
+        
+    }
+}
